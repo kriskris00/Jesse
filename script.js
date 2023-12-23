9c1bc110c284e2537b99096493e6b2f819e6d028
kriskris00 <127379387+kriskris00@users.noreply.github.com>
2023-12-23 23:51:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // 获取元素
 const glowingTitle = document.getElementById('glowing-title');
-const emailImageContainer = document.querySelector('.image-container');
 
 // Function to change text color
 function changeColor() {
@@ -13,20 +12,4 @@ function getRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-// Add click event listener
-glowingTitle.addEventListener('click', changeColor);
-
-// 添加CSS样式
-glowingTitle.style.textShadow = '0 0 10px rgba(255, 255, 255, 0.5), 0 0 20px rgba(255, 255, 255, 0.5), 0 0 30px rgba(255, 255, 255, 0.5)';
-glowingTitle.style.cursor = 'pointer';
-
-// 为图片容器添加居中样式
-emailImageContainer.style.display = 'flex';
-emailImageContainer.style.alignItems = 'center';
-emailImageContainer.style.justifyContent = 'center';
-emailImageContainer.style.flexDirection = 'column';
+       
