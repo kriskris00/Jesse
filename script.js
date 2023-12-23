afb7f4927d70e5c8fc862a3b0ff87e8834ff3123
kriskris00 <127379387+kriskris00@users.noreply.github.com>
2023-12-24 00:03:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,4 +12,14 @@ function getRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
-       
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Add click event listener
+glowingTitle.addEventListener('click', changeColor);
+
+// 添加CSS样式
+glowingTitle.style.textShadow = '0 0 10px rgba(255, 255, 255, 0.5), 0 0 20px rgba(255, 255, 255, 0.5), 0 0 30px rgba(255, 255, 255, 0.5)';
+glowingTitle.style.cursor = 'pointer'; /* 添加鼠标指针样式 */
