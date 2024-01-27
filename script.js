c6fbc3115f83ddc84f179eee9d209841a25dade7
kriskris00 <127379387+kriskris00@users.noreply.github.com>
2024-01-27 21:46:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,39 +25,3 @@ glowingTitle.addEventListener('click', changeColor);
 glowingTitle.style.textShadow = '0 0 10px rgba(255, 255, 255, 0.5), 0 0 20px rgba(255, 255, 255, 0.5), 0 0 30px rgba(255, 255, 255, 0.5)';
 glowingTitle.style.cursor = 'pointer'; /* 添加鼠标指针样式 */
 
-// 添加图片渐显渐出效果
-image.style.opacity = 0;
-
-image.addEventListener('load', function() {
-    fadeIn(image);
-});
-
-function fadeIn(element) {
-    let opacity = 0;
-    const interval = setInterval(function() {
-        if (opacity < 1) {
-            opacity += 0.1;
-            element.style.opacity = opacity;
-        } else {
-            clearInterval(interval);
-            setTimeout(function() {
-                fadeOut(element);
-            }, 2000); // 2秒后再执行fadeOut
-        }
-    }, 200);
-}
-
-function fadeOut(element) {
-    let opacity = 1;
-    const interval = setInterval(function() {
-        if (opacity > 0) {
-            opacity -= 0.1;
-            element.style.opacity = opacity;
-        } else {
-            clearInterval(interval);
-            setTimeout(function() {
-                fadeIn(element);
-            }, 2000); // 2秒后再执行fadeIn
-        }
-    }, 200);
-}
